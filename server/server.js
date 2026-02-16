9aaf92ba11a68c105e56182a1e87bf32029428f1
Kishore Raj <kishoreraj.2709@gmail.com>
2026-02-16 08:13:14 +0530

    Fixed client package.json for deployment

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const multer = require("multer");
 const cloudinary = require("./cloudinary");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -179,6 +183,7 @@ app.get("/",(req,res)=>{
 res.send("Watch Party Server Running 🚀");
 });
 
-server.listen(5000,()=>{
-console.log("🔥 Server running on port 5000");
+server.listen(PORT, () => {
+  console.log(`🔥 Server running on port ${PORT}`);
 });
+
